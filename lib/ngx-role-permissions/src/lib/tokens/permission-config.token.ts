29162618583314d6fc6932a383c71e1019a3bee9
DESKTOP-DA70CJK\Artem.Andreev <Artem.Andreev@clouty.ru>
2019-04-25 17:19:43 +0300

    add token type definition

--- lib/ngx-role-permissions/src/lib/tokens/permission-config.token.ts
+++ lib/ngx-role-permissions/src/lib/tokens/permission-config.token.ts
@@ -1,3 +1,4 @@
 import { InjectionToken } from '@angular/core';
+import { PermissionElement } from '../models/element';
 
-export const PERMISSION_CONFIG_TOKEN = new InjectionToken('PERMISSION_CONFIG_TOKEN');
+export const PERMISSION_CONFIG_TOKEN = new InjectionToken<PermissionElement[]>('PERMISSION_CONFIG_TOKEN');
